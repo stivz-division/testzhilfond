05896b26cb9e0bbe4eb818ec669eeed648c05723
Александр <stivz-division@mail.ru>
2023-07-06 19:18:07 +1100

    feat(auth): add surname create user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
 
         $user = User::create([
             'name' => $request->name,
+            'surname' => $request->surname,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
