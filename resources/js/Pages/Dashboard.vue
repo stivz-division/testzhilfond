b54b28a6d8a0941ff4d1143b82136a8b8719a22c
Александр <stivz-division@mail.ru>
2023-07-06 19:31:19 +1100

    feat(auth): add logout button

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,11 +1,21 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <div>
-        AUTH PAGE
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+    >
+        <span class="mr-2 text-white">
+            AUTH PAGE
+        </span>
+
+        <Link
+            :href="route('logout')"
+            method="post"
+            class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+        >Logout</Link>
     </div>
 </template>
