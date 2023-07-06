f11821bd429270ff8d33ebdb79fca526db487100
Александр <stivz-division@mail.ru>
2023-07-06 20:34:12 +1100

    feat(logger): add auth_logs table and model

---
+++ database/migrations/2023_07_06_085435_create_auth_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('auth_logs', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['login', 'registered']);
+            $table->string('message');
+            $table->ipAddress();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('auth_logs');
+    }
+
+};
